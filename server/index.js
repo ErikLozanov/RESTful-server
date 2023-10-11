2cbec4ec5a006e5d746ec29dae94abe1cf341551
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-11 11:02:05 +0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('RESTful service');
+});
+
+app.use(routes);
+app.listen(3030, () => console.log('RESTful server is listening on port 3030'));
