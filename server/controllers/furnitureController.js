11eb35085aa9bac1725ee80f183c1cd8d04b100a
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-11 12:57:56 +0300

    Add getAll furnitures

--- server/controllers/furnitureController.js
+++ server/controllers/furnitureController.js
@@ -1,4 +1,27 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+const furnitureManager = require("../managers/furnitureManager");
+
+router.get('/', async (req, res) => {
+    const furnitures = await furnitureManager.getAll();
+
+    res.json(furnitures);
+})
+
+router.post("/", async (req, res) => {
+    try {
+        await furnitureManager.create({
+            ...req.body,
+            _ownerId: req.user._id
+        });
+
+        res.status(204).end();
+    } catch (err) {
+        res.status(400).json({
+            message: 'Cannot create furniture'
+        });
+    }
+
+});
 
 module.exports = router;
