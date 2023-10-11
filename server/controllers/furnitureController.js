54591fd3a31f3a24b525b3d3e4dee7bbad4ec4ff
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-11 13:38:12 +0300

    Added Edit and Delete function for furnitures

--- server/controllers/furnitureController.js
+++ server/controllers/furnitureController.js
@@ -27,6 +27,36 @@ router.get('/:furnitureId',async (req, res) => {
     const furniture = await furnitureManager.getOne(req.params.furnitureId);
 
     res.json(furniture);
-})
+});
+
+router.put('/:furnitureId', async (req, res) => {
+
+    try {
+        await furnitureManager.update(req.params.furnitureId, req.body);
+
+        res.status(204).end();
+    } catch (err) {
+        res.status(400).json({
+            message: 'Cannot edit furniture'
+        });
+
+    }
+
+});
+
+router.delete('/:furnitureId', async (req, res) => {
+
+    try {
+        await furnitureManager.delete(req.params.furnitureId);
+
+        res.status(204).end();
+    } catch (err) {
+        res.status(400).json({
+            message: 'Cannot delete furniture'
+        });
+
+    }
+
+});
 
 module.exports = router;
